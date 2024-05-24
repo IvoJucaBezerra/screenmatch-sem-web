3b2ddeda56d7dce58012cd6ed44dd2a85a42735c
Ivo Juca <ivojucabezerra@hotmail.com>
2024-05-24 19:32:43 -0300

    Inicio do projeto spring

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,11 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//para ignorar o que não foi pedido (queremos apenas Title, totalSeason e imdbrRating)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+}
